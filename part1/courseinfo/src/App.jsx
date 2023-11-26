0c47dc8d33b8f7de46d97179e668dbe2120ad511
Wathsala <64484342+wathsala-vishwajith@users.noreply.github.com>
2023-11-26 19:39:24 +0530

    1-3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,23 +1,26 @@
-import Content from "./Content"
-import Header from "./Header"
-import Total from "./Total"
+import Content from "./Content";
+import Header from "./Header";
+import Total from "./Total";
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = "Half Stack application development";
+  const part1 = { name: "Fundamentals of React", exercises1: 10 };
+  const part2 = { name: "Using props to pass data", exercises2: 7 };
+  const part3 = { name: "State of a component", exercises3: 14 };
 
   return (
     <div>
       <Header course={course} />
-      <Content items={[[part1,exercises1],[part2,exercises2],[part3,exercises3]]} />
-      <Total exercises={[exercises1,exercises2,exercises3]}/>
+      <Content
+        items={[
+          [part1, exercises1],
+          [part2, exercises2],
+          [part3, exercises3],
+        ]}
+      />
+      <Total exercises={[exercises1, exercises2, exercises3]} />
     </div>
-  )
-}
+  );
+};
 
+export default App;
 
-export default App
