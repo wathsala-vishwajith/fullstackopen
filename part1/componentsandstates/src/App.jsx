21bf9027bf1f13e91c53cf87832a2447c2ac46fe
Wathsala <64484342+wathsala-vishwajith@users.noreply.github.com>
2023-11-28 14:46:44 +0530

    deconstructing

--- part1/componentsandstates/src/App.jsx
+++ part1/componentsandstates/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Button from "./Button";
+import Display from "./Display";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Display counter={counter} />
 
-export default App
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
+    </div>
+  );
+};
+export default App;
+
---
+++ part1/componentsandstates/src/Display.jsx
@@ -0,0 +1,4 @@
+const Display = (props) => {
+  return <div>{props.counter}</div>;
+};
+export default Display;
