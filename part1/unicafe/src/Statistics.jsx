45486507774f5d4982e81650654d172b54698c5c
Wathsala <64484342+wathsala-vishwajith@users.noreply.github.com>
2023-11-30 20:03:12 +0530

    1.9

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -5,17 +5,20 @@ const Statistics = ({ setValues }) => {
   const positive = (array) => {
     return ((array[0] * 1 + array[2] * -1) * 100) / sum(array);
   };
-
-  return (
-    <>
-      <p> good {setValues[0]} </p>
-      <p> neutral {setValues[1]}</p>
-      <p> bad {setValues[2]}</p>
-      <p> All {sum(setValues)} </p>
-      <p>Average {average(setValues)}</p>
-      <p>Positive {positive(setValues)}%</p>
-    </>
-  );
+  if (sum(setValues) === 0) {
+    return <p>No feedback given</p>;
+  } else {
+    return (
+      <>
+        <p> good {setValues[0]} </p>
+        <p> neutral {setValues[1]}</p>
+        <p> bad {setValues[2]}</p>
+        <p> All {sum(setValues)} </p>
+        <p>Average {average(setValues)}</p>
+        <p>Positive {positive(setValues)}%</p>
+      </>
+    );
+  }
 };
 
 export default Statistics;
